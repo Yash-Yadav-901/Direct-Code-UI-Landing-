c5dc44ecf042749d1ff8a0112fb16e1db2d87e6f
Yash Yadav <yashyadav65378@gmail.com>
2025-11-14 23:58:08 +0530

    refactor: remove unused App component from project

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import {createBrowserRouter, RouterProvider, createRoutesFromElements,Route} from 'react-router-dom'
 import MainLayout from './layout/Mainlayout.jsx'
 import Bg from './components/mainElements/Bg.jsx'
